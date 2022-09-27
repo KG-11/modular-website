a5e57375f47f460f5f7090add34edcf8da665aee
kg-11 <krishna.guru09@gmail.com>
2022-09-28 01:10:38 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,95 @@
+import Header from "./components/header";
+
+import bgVideo from "./assets/videos/home.mp4";
+
+import "./App.scss";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <video className="bg-video" src={bgVideo} autoPlay muted loop></video>
+      <main>
+        <section className="landing">
+          <h2 className="title">Lorem ipsum dolor sit amet.</h2>
+          <h3 className="title">Lorem ipsum dolor sit amet.</h3>
+        </section>
+        <section className="categories">
+          <div className="category">
+            <p className="desc-number">No.1</p>
+            <p className="desc-title">Lorem, ipsum dolor.</p>
+            <p className="desc">Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="category">
+            <p className="desc-number">No.2</p>
+            <p className="desc-title">Lorem, ipsum dolor.</p>
+            <p className="desc">Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="category">
+            <p className="desc-number">No.3</p>
+            <p className="desc-title">Lorem, ipsum dolor.</p>
+            <p className="desc">Lorem ipsum dolor sit amet.</p>
+          </div>
+        </section>
+        <section className="category-one">
+          <div className="category-heading-row">
+            <p>No.1</p>
+            <p>Lorem, ipsum dolor.</p>
+            <p>Lorem, ipsum dolor.</p>
+          </div>
+          <div className="category-qa">
+            <p className="q">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, fugit
+              ?
+            </p>
+            <p className="a">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus
+              corrupti voluptatem ad ipsum. Dolore facere nemo accusamus, earum
+              laboriosam ipsa. Lorem ipsum dolor sit, amet consectetur
+              adipisicing elit. Natus corrupti voluptatem ad ipsum. Dolore
+              facere nemo accusamus, earum laboriosam ipsa. laboriosam ipsa.
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus
+              corrupti voluptatem ad ipsum. Dolore facere nemo accusamus, earum
+              laboriosam ipsa. laboriosam ipsa. Lorem ipsum dolor sit, amet
+              consectetur adipisicing elit. Natus corrupti voluptatem ad ipsum.
+              Dolore facere nemo accusamus, earum laboriosam ipsa.
+            </p>
+          </div>
+          <div className="category-description">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus
+              corrupti voluptatem ad ipsum. Dolore facere nemo accusamus, earum
+              laboriosam ipsa. Lorem ipsum dolor sit, amet consectetur
+              adipisicing elit. Natus corrupti voluptatem ad ipsum. Dolore
+              facere nemo accusamus, earum laboriosam ipsa. laboriosam ipsa.
+              Lorem ipsum dolor sit.
+            </p>
+            <p>
+              sLorem ipsum dolor sit, amet consectetur adipisicing elit. Natus
+              corrupti voluptatem ad ipsum. Dolore facere nemo accusamus, earum
+              laboriosam ipsa. Lorem ipsum dolor sit, amet consectetur
+              adipisicing elit. Natus corrupti voluptatem ad ipsum. Dolore
+              facere nemo accusamus, earum laboriosam ipsa. laboriosam ipsa.
+              Lorem ipsum dolor sit.
+            </p>
+          </div>
+          <div className="category-options">
+            <p className="heading">Lorem, ipsum dolor.</p>
+            <a href="" className="link">
+              Lorem ipsum dolor sit.
+            </a>
+            <a href="" className="link">
+              Lorem ipsum dolor sit.
+            </a>
+            <a href="" className="link">
+              Lorem ipsum dolor sit.
+            </a>
+          </div>
+        </section>
+      </main>
+      <footer></footer>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/header/index.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { RiCloseCircleFill, RiMenu4Fill } from "react-icons/ri";
+import useWindowSize from "../../hooks/useWindowSize";
+
+import "./style.scss";
+
+const Header = () => {
+  const { width } = useWindowSize();
+  const [showMenu, toggleMenu] = useState(false);
+  return (
+    <header>
+      <nav className="nav">
+        <h1 className="nav-brand">Modular</h1>
+        {((width <= 991 && showMenu) || width > 991) && (
+          <ul className={`nav-list ${showMenu ? "responsive" : ""}`.trim()}>
+            <li
+              className={`nav-list-item ${width > 991 ? "ml-auto" : ""}`.trim()}
+            >
+              Live
+            </li>
+            <li className="nav-list-item">Work</li>
+            <li className="nav-list-item">Gallery</li>
+            <li className="nav-list-item">Studio</li>
+            <li className="nav-list-item">Relax</li>
+            <li className="nav-list-item">About</li>
+            <li className="nav-list-item">Sound</li>
+            <li className="nav-list-item">Contact</li>
+          </ul>
+        )}
+        {width <= 991 && (
+          <button onClick={() => toggleMenu(!showMenu)}>
+            {showMenu ? (
+              <RiCloseCircleFill className="menu-icon" />
+            ) : (
+              <RiMenu4Fill className="menu-icon" />
+            )}
+          </button>
+        )}
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
