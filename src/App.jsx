a039f625530ed0a71696bd0e4ec46cf8ec9279c1
Darkwolf007 <31916350+Darkwolf007@users.noreply.github.com>
2022-10-03 11:44:13 +0530

    style update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import Header from "./components/header";
 
 import bgVideo from "./assets/videos/home.mp4";
-import fullImage from "./assets/images/360-preview.webp";
+import fullImage from "./assets/images/render1.webp";
+import Gallery1 from "./assets/images/render3.webp";
 
 import "./App.scss";
 
@@ -12,84 +13,104 @@ const App = () => {
       <video className="bg-video" src={bgVideo} autoPlay muted loop></video>
       <main>
         <section className="landing">
-          <h2 className="title">Lorem ipsum dolor sit amet.</h2>
-          <h3 className="title">Lorem ipsum dolor sit amet.</h3>
+          <h2 className="title">New way of living.</h2>
+          <h3 className="title">
+            Change the way you live life, closer to nature.
+          </h3>
         </section>
         <section className="categories">
           <div className="category">
             <p className="desc-number">No.1</p>
-            <p className="desc-title">Lorem, ipsum dolor.</p>
-            <p className="desc">Lorem ipsum dolor sit amet.</p>
+            <p className="desc-title">Live in modular</p>
+            <p className="desc">Learn more about module to live in.</p>
           </div>
           <div className="category">
             <p className="desc-number">No.2</p>
-            <p className="desc-title">Lorem, ipsum dolor.</p>
-            <p className="desc">Lorem ipsum dolor sit amet.</p>
+            <p className="desc-title">Work from modular</p>
+            <p className="desc">Learn more about module to work from.</p>
           </div>
           <div className="category">
             <p className="desc-number">No.3</p>
-            <p className="desc-title">Lorem, ipsum dolor.</p>
-            <p className="desc">Lorem ipsum dolor sit amet.</p>
+            <p className="desc-title">Relax at modular</p>
+            <p className="desc">Learn more about module to relax at.</p>
           </div>
         </section>
         <section className="category-one">
           <div className="category-heading-row">
             <p>No.1</p>
-            <p>Lorem, ipsum dolor.</p>
-            <p>Lorem, ipsum dolor.</p>
+            <p>Our philosophy</p>
+            <p>Back to nature</p>
           </div>
           <div className="category-qa">
-            <p className="q">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, fugit
-              ?
-            </p>
+            <p className="q">What's Modular ?</p>
             <p className="a">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus
-              corrupti voluptatem ad ipsum. Dolore facere nemo accusamus, earum
-              laboriosam ipsa. Lorem ipsum dolor sit, amet consectetur
-              adipisicing elit. Natus corrupti voluptatem ad ipsum. Dolore
-              facere nemo accusamus, earum laboriosam ipsa. laboriosam ipsa.
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus
-              corrupti voluptatem ad ipsum. Dolore facere nemo accusamus, earum
-              laboriosam ipsa. laboriosam ipsa. Lorem ipsum dolor sit, amet
-              consectetur adipisicing elit. Natus corrupti voluptatem ad ipsum.
-              Dolore facere nemo accusamus, earum laboriosam ipsa.
+              Modular are prefabricated, fully finished, modular units. By a
+              lake, deep in a forest, at the edge of a field, a steep mountain
+              hill or your city rooftop! Make your dream come true, installed in
+              one day, no extra on-site work.
             </p>
           </div>
           <div className="category-description">
             <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus
-              corrupti voluptatem ad ipsum. Dolore facere nemo accusamus, earum
-              laboriosam ipsa. Lorem ipsum dolor sit, amet consectetur
-              adipisicing elit. Natus corrupti voluptatem ad ipsum. Dolore
-              facere nemo accusamus, earum laboriosam ipsa. laboriosam ipsa.
-              Lorem ipsum dolor sit.
+              The shelters are built in one piece, which gives the incredible
+              mobility to reach your dream location. Modularity ensures that
+              over time you can grow your Ark to your needs. The units are
+              created by craftsmen in our factory.
             </p>
             <p>
-              sLorem ipsum dolor sit, amet consectetur adipisicing elit. Natus
-              corrupti voluptatem ad ipsum. Dolore facere nemo accusamus, earum
-              laboriosam ipsa. Lorem ipsum dolor sit, amet consectetur
-              adipisicing elit. Natus corrupti voluptatem ad ipsum. Dolore
-              facere nemo accusamus, earum laboriosam ipsa. laboriosam ipsa.
-              Lorem ipsum dolor sit.
+              We have ideal conditions and are able to reach the highest details
+              and reduce production time. Which allows us to keep control of the
+              price, remove any onsite construction constraints and make your
+              dream home, on time, on budget, at the highest quality possible.
             </p>
           </div>
           <div className="category-options">
-            <p className="heading">Lorem, ipsum dolor.</p>
+            <p className="heading">Choose your purpose.</p>
             <a href="" className="link">
-              Lorem ipsum dolor sit.
+              Module to live in.
             </a>
             <a href="" className="link">
-              Lorem ipsum dolor sit.
+              Module as an office.
             </a>
             <a href="" className="link">
-              Lorem ipsum dolor sit.
+              Module to relax in.
             </a>
           </div>
         </section>
         <section className="preview">
           <img src={fullImage} alt="360 preview" />
           <button className="btn-white rounded">Take a look around</button>
+        </section>
+        {/* \\\\\\\\\\\\\\\ */}
+
+        <section className="category-two">
+          <div className="category-heading-row">
+            <p>No.2</p>
+            <p>Lorem, ipsum dolor.</p>
+            <p>Lorem, ipsum dolor.</p>
+          </div>
+          <div className="category-qa">
+            <p className="q">
+              <h1>Find a space to unwind</h1>
+              <h4>
+                Ark-Shelter shows the beauty of natural light and allows you to
+                reconnect with nature. The large single pane windows seamlessly
+                integrate the natural environment within your Ark. Enjoy a
+                moment for yourself and take time to unwind.
+              </h4>
+
+              <h1>Connect with nature</h1>
+              <h4>
+                Due to Ark-Shelter’s self-sufficient system and ecological
+                materials, you’ll live with zero-impact. Become as productive as
+                nature itself, and grow and bloom within your own Ark.
+              </h4>
+            </p>
+
+            <p className="a">
+              <img src={Gallery1} alt="360 preview" />
+            </p>
+          </div>
         </section>
       </main>
       <footer></footer>
