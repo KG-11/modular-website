cf91742fe1804c8b5e01d411340a6887fb67772f
Shylesh Kumar <shyleshkumar4012@gmail.com>
2022-10-12 18:17:29 +0530

    Content

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -86,8 +86,8 @@ const Home = () => {
         <section className="category-two">
           <div className="category-heading-row">
             <p>No.2</p>
-            <p>Lorem, ipsum dolor.</p>
-            <p>Lorem, ipsum dolor.</p>
+            <p>Why Modular?</p>
+            <p>Back to nature</p>
           </div>
           <div className="category-qa">
             <p className="q">
@@ -141,31 +141,50 @@ const Home = () => {
         </section>
         <section className="category-three">
           <div className="category-heading-row">
-            <p>No.2</p>
-            <p>Lorem, ipsum dolor.</p>
-            <p>Lorem, ipsum dolor.</p>
+            <p>No.3</p>
+            <p>About Modular</p>
+            <p>Back to nature</p>
+          </div>
+
+          <div className="b">
+            <h1>
+              Our Arks are manufactured by carefully selected and trained
+              craftsmen. The high-quality wood we use for the base structure is
+              durable and designed to be resistant in any environment.
+            </h1>
           </div>
           <div className="category-qa">
             <p className="q">
-              <h2>Find a space to unwind</h2>
+              <h4>Connect with nature</h4>
               <h4>
-                Ark-Shelter shows the beauty of natural light and allows you to
-                reconnect with nature. The large single pane windows seamlessly
-                integrate the natural environment within your Ark. Enjoy a
-                moment for yourself and take time to unwind.
-              </h4>
-
-              <h2>Connect with nature</h2>
-              <h4>
-                Due to Ark-Shelter’s self-sufficient system and ecological
-                materials, you’ll live with zero-impact. Become as productive as
-                nature itself, and grow and bloom within your own Ark.
+                Ark-Shelter began with one question: how will people live in the
+                future? Architecture students Michiel and Martin came up with
+                the first concept of the Ark-Shelter, a compact house with a
+                clean design. It’s a reinvention of how people live their lives
+                in today’s fast paced and busy world by going back to nature and
+                back to basics. The concept has one purpose, allow people to
+                come to themselves, reconnect with nature and reconnect with
+                themselves.
               </h4>
             </p>
 
             <p className="a">
               <img src={Gallery1} alt="360 preview" />
             </p>
+          </div>
+        </section>
+        {/* /////////// */}
+        <section className="landing">
+          <h2 className="title">New way of living.</h2>
+          <h3 className="title">
+            Change the way you live life, closer to nature.
+          </h3>
+        </section>
+        <section className="categories">
+          <div className="category">
+            <p className="desc-number">No.1</p>
+            <p className="desc-title">Live in modular</p>
+            <p className="desc">Learn more about module to live in.</p>
           </div>
         </section>
       </main>
